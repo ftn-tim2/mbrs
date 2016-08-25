b3b106abfa47fc6b4eea0514d12319c511e120d9
Berko Szabolcs <szberko@gmail.com>
2016-08-25 22:26:11 +0200

    add javadoc and change python3 to python

--- src/myplugin/GenerateDjango.java
+++ src/myplugin/GenerateDjango.java
@@ -9,15 +9,26 @@ import java.util.Scanner;
  */
 public class GenerateDjango {
 
+
+    /**
+     * Need to check if there more than one python installed on the system. If there is multiple python,
+     * in most cases it means that Python 2.7 will have keyword "python" and Python 3.xx will have 'python3'.
+     *
+     * Recommended and minimum python version is: Python 3.xx.
+     *
+     * @param dslLocation absolute path of file which contains program code written in jsd DSL language
+     * @param dslFileName name of the file which contains program code written in jsd DSL language
+     * @param outputLocation absolute path of folder where will be the code generated
+     */
     public static void generateDjango(String dslLocation, String dslFileName, String outputLocation) {
         try {
 
             ProcessBuilder pb = null;
             if(System.getProperty("os.name").contains("Mac") || System.getProperty("os.name").contains("Linux")) {
-                pb = new ProcessBuilder("python3", "generateDjangoUnix.py", dslLocation, dslFileName, outputLocation);
+                pb = new ProcessBuilder("python", "generateDjangoUnix.py", dslLocation, dslFileName, outputLocation);
 
             }else if (System.getProperty("os.name").contains("Windows")) {
-                pb = new ProcessBuilder("python3", "generateDjangoWindows.py", dslLocation, dslFileName, outputLocation);
+                pb = new ProcessBuilder("python", "generateDjangoWindows.py", dslLocation, dslFileName, outputLocation);
             }
 
             Process p = pb.start();
